1a5221ea14b21ac8b56b884a3e0655ff8139a0c9
fanaman74 <fredanaman@gmail.com>
2026-04-08 13:06:03 +0200

    feat: add Hero section with video background and entrance animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { OrderProvider } from './context/OrderContext';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 export default function App() {
   return (
     <OrderProvider>
       <div className="min-h-screen bg-bg text-text font-body">
         <Navbar />
+        <Hero />
       </div>
     </OrderProvider>
   );
